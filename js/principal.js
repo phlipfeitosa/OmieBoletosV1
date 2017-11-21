15160ddecba19a48f5b230596b2b513226048405
phlipfeitosa <philip.feitosa@hotmail.com>
2017-11-21 16:09:34 -0200

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,19 @@
 /**/
+var dataSend = '{{"call":"ConsultarBanco","app_key":"1560731700","app_secret":"226dcf372489bb45ceede61bfd98f0f1","param":[{"codigo":1}]}}';
+
+$('#buscar').click(function() {
+	$.ajax({
+		url:'http://app.omie.com.br/api/v1/geral/bancos/',
+		contentType: "application/json; charset=utf-8",
+		dataType:'json',
+		crossDomain:true,
+		type:'POST',
+		data:dataSend,
+		success:function(data) {
+			console.log('BUSCOU COM AJAX');
+		},
+		error:function(data) {
+			console.log(data);
+		}
+	});
+});
